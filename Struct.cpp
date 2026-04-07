f443a44050bb0f5f933abc8ef048ec5fb20ed6c8
yara22aulia <yara22aulia@gmail.com>
2026-04-07 15:28:37 +0700

    menampilkan isi obyek

---
+++ Struct.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct orang{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main(
+)
+{
+    // Deklarasi obyek dari struct orang
+    orang mhs;
+    // Isi objek
+    cout << "Nama Mahasiswa:";
+    cin >> mhs.nama;
+    cout << "Alamat Asal:";
+    cin >> mhs.alamat;
+    cout << "Umur:";
+    cin >> mhs.umur;
+    cout << endl;
+
+    // Menampilkan Isi Obyek
+    cout << "Nama   : " << mhs.nama<<endl;
+    cout << "Alamat : " << mhs.alamat<<endl;
+    cout << "Umur   : " << mhs.umur<<endl;
+}
