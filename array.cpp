103ca1675115f3a2e35dd836c1b5ea4c12218b72
yara22aulia <yara22aulia@gmail.com>
2026-04-07 15:26:31 +0700

    cara membaca array

--- array.cpp
+++ array.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     // Deklarasi Array dengan inisialisasi
     int dataku[5] = {10, 20, 30, 40, 50};
     // Deklarasi tanpa inisialisasi
